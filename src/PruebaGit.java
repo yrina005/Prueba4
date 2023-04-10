15edfb95dc4c78d8f976af34e1a44dd0e619347a
yrina005 <129984859+yrina005@users.noreply.github.com>
2023-04-10 16:28:14 -0300

    Update PruebaGit.java
    Corrijo punto y coma

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -5,8 +5,8 @@ public class PruebaGit {
 		// TODO Auto-generated method stub
 		int a=1;
 		int b=2;
-		double promedio = (a+b)/2
-		System.out.println(promedio)
+		double promedio = (a+b)/2;
+		System.out.println(promedio);
 
 	}
 
